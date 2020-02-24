be5d647528bef92c30775cb1d814eb6443010709
Nathan C <sihoonathan@gmail.com>
2020-02-23 20:40:36 -0800

    bubble_sort_by added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,36 @@
 def bubble_sort(array)
   for m in (0...array.length - 1)
-    $sorted = false
+    sorted = false
     for i in (0...array.length-1-m)
       if array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
-        $sorted = true
+        sorted = true
       end
     end
-    break unless $sorted
+    break unless sorted
   end
   array
 end
 
-p bubble_sort([1, 2, 3])
+
+def bubble_sort_by(array)
+  for m in (0...array.length - 1)
+    sorted = false
+    for i in (0...array.length-1-m)
+      if yield(array[i], array[i+1]) > 0
+        array[i], array[i+1] = array[i+1], array[i]
+        sorted = true
+      end
+    end
+    break unless sorted
+  end
+  array
+end
+
 p bubble_sort([4,3,78,2,0,2])
+
+p (bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
+end)
+
 
