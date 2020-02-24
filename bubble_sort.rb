b5f54dee4d142b732741c2a1c596c8795ed9006c
Nathan C <sihoonathan@gmail.com>
2020-02-23 19:00:45 -0800

    partially completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,12 @@
-p "hey"
-p "added something"
+def bubble_sort(array)
+  for m in (0...array.length - 1)
+    for i in (0...array.length-1)
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+      end
+    end
+  end
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
